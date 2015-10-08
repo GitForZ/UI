0a254d92685c056ca0c286b73e3bfa3ec806551d
Marcus Lorenzana <lorenzanamarcus@gmail.com>
2015-10-08 00:34:22 -0500

    Budget update project: working authentication, User with budgets relationship, Create budget, edit budget, have many budgets per user, MVC

---
+++ quota/resources/views/budgets/index.blade.php
@@ -0,0 +1,18 @@
+@extends('app'); 
+
+@section('content')
+
+
+<h1>Budgets</h1>
+
+
+
+    @for ($i = 0; $i < count($budgets); $i++)
+        <div class ="body">
+            <a href="{{ url('/budgets',$budgets[$i]->id) }}"><h3>Budget:{{$array[$i]}}</h3></a>
+        </div>
+    @endfor
+    
+
+@stop
+
