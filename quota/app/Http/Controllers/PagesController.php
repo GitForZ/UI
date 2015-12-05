8bae001f8f05bdbbb82aef0ffd0ce04c95a0ac63
Marcus Lorenzana <lorenzanamarcus@gmail.com>
2015-12-05 01:59:07 -0600

    fixing accidental deletions

---
+++ quota/app/Http/Controllers/PagesController.php
@@ -0,0 +1,40 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller {
+
+	public function about() {
+		#Passing variable to a view using compact
+		/*
+		$first = 'Marcus';
+		$last = 'Lorenzana'; 
+		return view('pages.about',compact('first','last')); 
+		*/
+		
+		$features = [
+			'feature1','feature2','feature3'
+		];
+		
+		return view('pages.about',compact('features')); 
+		
+		#Passing variable to a view using associative array
+		/*
+		$data = [];
+		$data['first']='Marcus';
+		$data['last']='Lorenzana';
+		return view('pages.about',$data);
+		*/
+	}
+	
+	public function contact() {
+		
+		return view('pages.contact'); 
+		
+	}
+	
+
+}
